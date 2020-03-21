600e152289e469015644cb70651ac867f9b2b1c0
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2020-03-21 09:41:11 -0500

    Add files via upload

--- country_info.py
+++ country_info.py
@@ -8,17 +8,20 @@ import matplotlib.style
 import matplotlib as mpl
 import datetime
 import csv
-	
+
 args = sys.argv;
 path = 'covid-19_confirmed.csv'
 ytitle = 'confirmed cases'
 type = 'log'
+# specify list of countries to track
 clist = ['Sweden','Switzerland','US','Korea, South','Italy','Spain','France','United Kingdom']
 #clist = ['Sweden','Denmark','Canada','Norway']
+# if the 2nd command line argument is 'linear' or 'log', use that to set y-axis type
 try:
 	type = args[2]
 except:
 	type = 'log'
+# select either confirmed cases or deaths for graph, from the 1st command line argument
 try:
 	if args[1] == 'deaths':
 		path = 'covid-19_deaths.csv'
@@ -33,6 +36,7 @@ except:
 	ytitle = 'confirmed cases'
 
 lines = []
+# read the csv file into a list of lists
 with open(path) as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=',')
 	for r in readCSV:
@@ -41,8 +45,10 @@ with open(path) as csvfile:
 
 titles = lines[0]
 lines = lines[1:]
+#create an x-axis list, converting the dates in the first line into datetime objects
 xs = [datetime.datetime.strptime(d,"%m/%d/%y").date() for d in titles[4:]]
 country = {}
+#parse lines into a dict of country totals
 for l in lines:
 	c = l[1]
 	if c not in country:
@@ -54,18 +60,18 @@ for l in lines:
 				country[c][i] += int(t)
 			except:
 				pass
-plt.yscale('log')
 mpl.style.use('seaborn-notebook')
 ax = plt.gca()
 plt.yscale(type)
 plt.ylabel(ytitle)
+#set titles based on the csv used
 xtitle = 'days after 10th death'
 if ytitle == 'confirmed cases':
 	xtitle = 'days after 100th case'
 plt.xlabel(xtitle)
 plt.title('COVID-19 %s (Johns Hopkins CSSE data)' % (ytitle))
 ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
-
+#go through the selected country data and organize into plotted graphs
 for c in country:
 	if c not in clist:
 		continue
@@ -89,6 +95,7 @@ for c in country:
 		continue
 	plt.text(len(xv)-1+.2,yv[-1],c)
 	plt.plot(xv,yv,marker='o',label='%s' % (c))
+#show the graph
 plt.legend(loc='best')
 plt.grid(True, lw = 1, ls = '--', c = '.8')
 fig = matplotlib.pyplot.gcf()
